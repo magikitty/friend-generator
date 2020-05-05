cfc3f6ce6742daba5300e8354f89f76182bc8e50
magikitty <elina.satola@gmail.com>
2020-05-05 19:10:58 +0300

    Modify main.go with constants and functions from utils

--- main.go
+++ main.go
@@ -3,21 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"friend-generator/utils"
 	"os"
 )
 
-const menuInstructions = "What do you want to do? Press the number of your choice."
-const messageWelcome = "Welcome to the Friend Generator!\n\n"
-
 func main() {
-	fmt.Print(messageWelcome)
-	menuMain()
-}
-
-func menuMain() {
-	fmt.Println(menuInstructions)
-	fmt.Println("1. Generate friend")
-	fmt.Println("2. Quit")
+	fmt.Println(utils.MessageWelcome)
+	fmt.Println(utils.MenuInstructions)
+	utils.PrintMessages(utils.MenuMainMessages, true)
 	getUserInput()
 }
 
