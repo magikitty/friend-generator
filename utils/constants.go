18146ac6d546fe9a8da1f8dd784a567a90e19385
magikitty <elina.satola@gmail.com>
2020-05-05 19:08:43 +0300

    Create constants.go with constant messages

---
+++ utils/constants.go
@@ -0,0 +1,17 @@
+package utils
+
+// MenuMainMessages contains messages and options for the main menu
+var MenuMainMessages = []string{
+	generateFriend,
+	quit,
+}
+
+// MenuInstructions explains how to make a selection in the menu
+const MenuInstructions = "What do you want to do? Press the number of your choice.\n"
+
+// Messages for the main menu
+var generateFriend = "Generate Friend"
+var quit = "Quit"
+
+// MessageWelcome greets user when they start the application
+const MessageWelcome string = "****** Welcome to the Friend Generator! ******\n\n"
