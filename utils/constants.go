6ad28f290685e9c63fc4725e71237763d454366c
magikitty <elina.satola@gmail.com>
2020-05-06 15:58:58 +0300

    Modify constants.go, modify PrintMenu so menu map items always print in the same correct order

--- utils/constants.go
+++ utils/constants.go
@@ -1,9 +1,9 @@
 package utils
 
 // MenuMainMessages contains messages and options for the main menu
-var MenuMainMessages = map[int]string{
-	1: generateFriend,
-	2: quit,
+var MenuMainMessages = map[string]string{
+	"1": generateFriend,
+	"2": quit,
 }
 
 // MenuInstructions explains how to make a selection in the menu
@@ -12,6 +12,9 @@ const MenuInstructions = "What do you want to do? Press the number of your choic
 // Messages for the main menu
 var generateFriend = "Generate Friend"
 var quit = "Quit"
+
+// SelectionUser for showing the user what they selected
+var SelectionUser = "You selected: "
 
 // MessageWelcome greets user when they start the application
 const MessageWelcome string = "****** (^ ^) Welcome to the Friend Generator! (^ ^) ******\n\n"
--- utils/helpers.go
+++ utils/helpers.go
@@ -1,14 +1,17 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // PrintMenu prints messages with or without a number
-func PrintMenu(menu map[int]string, showNumber bool) {
-	for key, menuItem := range menu {
+func PrintMenu(menu map[string]string, showNumber bool) {
+	for i := 1; i <= len(menu); i++ {
 		if showNumber == true {
-			fmt.Printf("%v. %s\n", key, menuItem)
+			fmt.Printf("%v. %s\n", i, menu[strconv.Itoa(i)])
 		} else {
-			fmt.Printf("%s\n", menuItem)
+			fmt.Printf("%s\n", menu[strconv.Itoa(i)])
 		}
 	}
 }
