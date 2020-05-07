829c2bf031066f928a46c1f46fddf863c789bdae
magikitty <elina.satola@gmail.com>
2020-05-07 11:00:16 +0300

    Move PrintMenu, GetMenuSelection and GetUserInput to new menu package

---
+++ menu/menu.go
@@ -0,0 +1,49 @@
+package menu
+
+import (
+	"bufio"
+	"fmt"
+	"friend-generator/utils"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// PrintMenu prints messages with or without a number
+func PrintMenu(menu map[string]string, showNumber bool) {
+	for i := 1; i <= len(menu); i++ {
+		if showNumber == true {
+			fmt.Printf("%v. %s\n", i, menu[strconv.Itoa(i)])
+		} else {
+			fmt.Printf("%s\n", menu[strconv.Itoa(i)])
+		}
+	}
+}
+
+// GetMenuSelection returns the map value corresponding to option (key) user selected
+func GetMenuSelection(menu map[string]string) (menuSelection string) {
+	getInput := true
+	for getInput == true {
+		input := GetUserInput()
+		_, ok := menu[input]
+		if ok == true {
+			getInput = false
+			menuSelection = menu[input]
+		} else {
+			fmt.Println(utils.MessageMenuWrongInput)
+		}
+	}
+	fmt.Println(utils.MessageSelectionUser, menuSelection)
+	return menuSelection
+}
+
+// GetUserInput returns user's input
+func GetUserInput() string {
+	reader := bufio.NewReader(os.Stdout)
+	userInput, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	inputTrimmed := strings.TrimSpace(userInput)
+	return inputTrimmed
+}
--- utils/helpers.go
+++ utils/helpers.go
@@ -1,17 +1,1 @@
 package utils
-
-import (
-	"fmt"
-	"strconv"
-)
-
-// PrintMenu prints messages with or without a number
-func PrintMenu(menu map[string]string, showNumber bool) {
-	for i := 1; i <= len(menu); i++ {
-		if showNumber == true {
-			fmt.Printf("%v. %s\n", i, menu[strconv.Itoa(i)])
-		} else {
-			fmt.Printf("%s\n", menu[strconv.Itoa(i)])
-		}
-	}
-}
