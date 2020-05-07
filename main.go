688adc4e9087214f9c7a82925fc3e9ca21ae5ae5
magikitty <elina.satola@gmail.com>
2020-05-07 15:15:34 +0300

    Modify callMenuMainFunctions and menuGenerator; create callMenuGeneratorFunctions, generateCustomFriend and generateRandomFriend

--- main.go
+++ main.go
@@ -19,19 +19,45 @@ func menuMain() {
 	callMenuMainFunctions(menuSelection)
 }
 
-func menuGenerator() {
-	fmt.Println(utils.MenuInstructions)
-	menu.PrintMenu(utils.MenuGeneratorOptions, true)
-	menu.GetMenuSelection(utils.MenuGeneratorOptions)
-}
-
 func callMenuMainFunctions(menuSelection string) {
 	switch menuSelection {
 	case utils.MenuMainOptions["1"]:
 		menuGenerator()
 	case utils.MenuMainOptions["2"]:
-		fmt.Println("You quit the program. Bye bye!") // debugging
+		quit()
 	default:
 		fmt.Println("Default option, nothing else to do") // debugging
 	}
+}
+
+func quit() {
+	fmt.Println(utils.MessageQuit)
+}
+
+func menuGenerator() {
+	fmt.Println(utils.MenuInstructions)
+	menu.PrintMenu(utils.MenuGeneratorOptions, true)
+	menuSelection := menu.GetMenuSelection(utils.MenuGeneratorOptions)
+	callMenuGeneratorFunctions(menuSelection)
+}
+
+func callMenuGeneratorFunctions(menuSelection string) {
+	switch menuSelection {
+	case utils.MenuGeneratorOptions["1"]:
+		generateRandomFriend()
+	case utils.MenuGeneratorOptions["2"]:
+		generateCustomFriend()
+	case utils.MenuGeneratorOptions["3"]:
+		menuMain()
+	default:
+		fmt.Println("Default option, nothing else to do") // debugging
+	}
+}
+
+func generateCustomFriend() {
+	fmt.Println("Generating a custom friend for you...") // debugging
+}
+
+func generateRandomFriend() {
+	fmt.Println("Generating a random friend for you...") // debugging
 }
