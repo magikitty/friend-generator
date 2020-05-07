62f079b5e1b5ff5668e375a37fe7d696a9670651
magikitty <elina.satola@gmail.com>
2020-05-07 15:13:38 +0300

    Modify and reorganize constants

--- utils/constants.go
+++ utils/constants.go
@@ -1,14 +1,8 @@
 package utils
 
-// MenuMainOptions contains messages and options for the main menu
-var MenuMainOptions = map[string]string{
-	"1": generateFriend,
-	"2": quit,
-}
-
-// Messages for the main menu
-var generateFriend = "Generate Friend"
-var quit = "Quit"
+/*******
+Messages
+********/
 
 // MenuInstructions explains how to make a selection in the menu
 const MenuInstructions = `
@@ -16,14 +10,34 @@ const MenuInstructions = `
 What do you want to do? Press the number of your choice.
 `
 
+// MessageQuit is a message for when the user quits the program
+const MessageQuit = "You quit the program. Bye bye!"
+
 // MessageWelcome greets user when they start the application
-const MessageWelcome string = "****** (^ ^) Welcome to the Friend Generator! (^ ^) ******"
+const MessageWelcome = "****** (^ ^) Welcome to the Friend Generator! (^ ^) ******"
+
+/********************
+Menu maps and options
+*********************/
+
+// MenuMainOptions contains messages and options for the main menu
+var MenuMainOptions = map[string]string{
+	"1": generateFriend,
+	"2": quit,
+}
+
+// Options for the main menu
+var generateFriend = "Generate Friend"
+var quit = "Quit"
 
 // MenuGeneratorOptions contains the options for generating a friend
 var MenuGeneratorOptions = map[string]string{
 	"1": randomFriend,
 	"2": customFriend,
+	"3": mainMenu,
 }
 
+// Options for the generator menu
 var randomFriend = "Generate random friend"
 var customFriend = "Generate custom friend"
+var mainMenu = "Main menu"
