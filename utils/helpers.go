9e1395ccee044407c9f97bd6c937809f5cd1d776
magikitty <elina.satola@gmail.com>
2020-05-11 14:10:54 +0300

    Move WriteDataToFile from main.go to utils/helpers.go, create ReadDataFromFile

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,1 +1,41 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// WriteDataToFile writes JSON data to a file
+func WriteDataToFile(data FriendResponse) {
+	file, err := os.OpenFile("./jsonresponses/test.json", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	jsondata, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = file.Write(jsondata)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// ReadDataFromFile reads and returns data from a JSON file
+func ReadDataFromFile() FriendResponse {
+	var friendResponse FriendResponse
+	jsondata, _ := ioutil.ReadFile("./jsonresponses/test.json")
+	err := json.Unmarshal(jsondata, &friendResponse)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("We read and unmarshaled:", friendResponse)                                 // debugging
+	fmt.Println("We read and unmarshaled gender result:", friendResponse.Results[0].Gender) // debugging
+
+	return friendResponse
+}
