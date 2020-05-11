7da0c4996068d5390740b238547ffd126d65f75b
magikitty <elina.satola@gmail.com>
2020-05-11 15:49:59 +0300

    Modify WriteDataToFile and ReadDataFromFile

--- utils/helpers.go
+++ utils/helpers.go
@@ -2,40 +2,38 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 // WriteDataToFile writes JSON data to a file
-func WriteDataToFile(data FriendResponse) {
-	file, err := os.OpenFile("./jsonresponses/test.json", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+func WriteDataToFile(data FriendResponse, jsonFile string) {
+	file, err := os.OpenFile(jsonFile, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	defer file.Close()
 
-	jsondata, err := json.MarshalIndent(data, "", " ")
+	jsonData, err := json.MarshalIndent(data, "", "   ")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
-	_, err = file.Write(jsondata)
+	_, err = file.Write(jsonData)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 }
 
 // ReadDataFromFile reads and returns data from a JSON file
-func ReadDataFromFile() FriendResponse {
+func ReadDataFromFile(jsonFile string) FriendResponse {
 	var friendResponse FriendResponse
-	jsondata, _ := ioutil.ReadFile("./jsonresponses/test.json")
-	err := json.Unmarshal(jsondata, &friendResponse)
+	jsonData, _ := ioutil.ReadFile(jsonFile)
+	err := json.Unmarshal(jsonData, &friendResponse)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println("We read and unmarshaled:", friendResponse)                                 // debugging
-	fmt.Println("We read and unmarshaled gender result:", friendResponse.Results[0].Gender) // debugging
 
 	return friendResponse
 }
