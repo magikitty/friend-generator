7d379177c219f25da0624dd5ff43b2466e5cfa58
magikitty <elina.satola@gmail.com>
2020-05-13 17:40:28 +0300

    Rename menu_generator.go to menu_generate_friend.go; implement generateRandomFriend

---
+++ menus/menu_generate_friend.go
@@ -0,0 +1,39 @@
+package menus
+
+import (
+	"errors"
+	"fmt"
+	"friend-generator/utils"
+
+	"github.com/magikitty/menu"
+)
+
+func menuGenerateFriend() {
+	fmt.Println(utils.MenuInstructions)
+	menu.PrintMenu(utils.MenuGenerateFriendOptions, true)
+	menuSelection := menu.GetMenuSelection(utils.MenuGenerateFriendOptions)
+	callMenuGenerateFriendFunctions(menuSelection)
+}
+
+func callMenuGenerateFriendFunctions(menuSelection string) {
+	switch menuSelection {
+	case utils.MenuGenerateFriendOptions["1"]:
+		generateRandomFriend()
+	case utils.MenuGenerateFriendOptions["2"]:
+		generateCustomFriend()
+	case utils.MenuGenerateFriendOptions["3"]:
+		MenuMain()
+	default:
+		errors.New(utils.ErrorInvalidSelection)
+	}
+}
+
+func generateCustomFriend() {
+	fmt.Println(utils.MessageNewFriend)
+}
+
+func generateRandomFriend() {
+	fmt.Println(utils.MessageNewFriend)
+	responseData := utils.GetJSONFriendData(utils.API)
+	utils.CreateAndPopulateFile(utils.JSONDataFilePath, responseData)
+}
