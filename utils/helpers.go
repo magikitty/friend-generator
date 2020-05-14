8b09ce464966283840788beb09cfeb3994b2f848
magikitty <elina.satola@gmail.com>
2020-05-14 14:54:09 +0300

    Create PrintNewFriendInfo, reorganize file

--- utils/helpers.go
+++ utils/helpers.go
@@ -2,10 +2,49 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
+
+// CreateAndPopulateFile creates file if it does not exist
+func CreateAndPopulateFile(filePath string, responseData FriendResponse) {
+	_, err := os.Stat(filePath)
+	if err != nil {
+
+		_, err := os.Create(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		friendsCollection := FriendsCollection{responseData.Results}
+		WriteDataToFile(friendsCollection, filePath)
+
+	} else {
+		friendsFromFile := GetFriendsFromFile(filePath)
+		friendsData := AppendFriends(responseData, friendsFromFile)
+		WriteDataToFile(friendsData, filePath)
+	}
+}
+
+// WriteDataToFile writes JSON data to a file
+func WriteDataToFile(data FriendsCollection, filePath string) {
+	file, err := os.OpenFile(filePath, os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 // AppendFriends appends new friends to friends collection
 func AppendFriends(responseData FriendResponse, friendsFromFile []Friend) FriendsCollection {
@@ -41,41 +80,12 @@ func ReadDataFromFile(filePath string) FriendsCollection {
 	return friendsCollection
 }
 
-// WriteDataToFile writes JSON data to a file
-func WriteDataToFile(data FriendsCollection, filePath string) {
-
-	file, err := os.OpenFile(filePath, os.O_WRONLY, os.ModePerm)
-	if err != nil {
-		log.Println(err)
-	}
-	defer file.Close()
-
-	jsonData, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		log.Println(err)
-	}
-
-	_, err = file.Write(jsonData)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-// CreateAndPopulateFile creates file if it does not exist
-func CreateAndPopulateFile(filePath string, responseData FriendResponse) {
-	_, err := os.Stat(filePath)
-	if err != nil {
-
-		_, err := os.Create(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		friendsCollection := FriendsCollection{responseData.Results}
-		WriteDataToFile(friendsCollection, filePath)
-
-	} else {
-		friendsFromFile := GetFriendsFromFile(filePath)
-		friendsData := AppendFriends(responseData, friendsFromFile)
-		WriteDataToFile(friendsData, filePath)
-	}
+// PrintNewFriendInfo prints out the information of the last friend in the file
+func PrintNewFriendInfo() {
+	friends := GetFriendsFromFile(JSONDataFilePath)
+	friend := friends[len(friends)-1]
+	fmt.Printf(Name, friend.Name.First, friend.Name.Last)
+	fmt.Printf(Age, friend.DOB.Age)
+	fmt.Printf(Gender, friend.Gender)
+	fmt.Printf(Location, friend.Location.City, friend.Location.Country)
 }
